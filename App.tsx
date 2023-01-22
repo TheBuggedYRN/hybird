640e8070d8de433097058cc1628b0125f6208922
Youssef Raafat <yraafat@instabug.com>
2023-01-22 14:05:50 +0200

    add react-native app

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import type {ReactNode} from 'react';
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App: () => ReactNode = () => {
+  return (
+    <SafeAreaView>
+      <Text>React Native App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
